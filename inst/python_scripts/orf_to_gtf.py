d0d2af7bb2b07b603c707b586f1ed1793ecdda3b
Chun Shen Lim <chunshen.lim@otago.ac.nz>
2025-08-18 19:58:03 +1200

    add orf_to_gtf.py

---
+++ inst/python_scripts/orf_to_gtf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+@author      CS Lim
+@create date 2025-08-18 17:23:00
+@modify date 2025-08-18 18:55:04
+@desc        RIBOSS module for generating flatten, ORF-centric GTF files
+"""
+
+import os
+import pickle
+import sys
+import argparse
+import pandas as pd
+import pyranges as pr
+import csv
+from riboss.orfs import orf_finder
+from riboss.gtf import flatten_orfs, bed6_to_bed12, bed12_to_gtf
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate flattened GTF for DOTSeq and featureCounts.")
+    parser.add_argument("--gtf", required=True, help="Path to GENCODE GTF file")
+    parser.add_argument("--orf_finder", help="Path to RIBOSS orf_finder pickle file")
+    parser.add_argument("--transcripts", help="Path to transcript FASTA file")
+    parser.add_argument("--output", required=True, help="Output file prefix")
+    parser.add_argument("--start_codon", default="ATG", help="Start codon to use")
+    parser.add_argument("--stop_codon", action="store_true", help="Require stop codon")
+
+    args = parser.parse_args()
+
+    if args.gtf and args.transcripts and not args.orf_finder:
+        cds_range, df = orf_finder(args.gtf, args.transcripts, ncrna=False, outdir=None,
+                                   start_codon=args.start_codon, stop_codon=args.stop_codon)
+    elif args.orf_finder:
+            df = pd.read_pickle(args.orf_finder)
+    else:
+        print("Error: --transcripts is required when using --gtf.", file=sys.stderr)
+        sys.exit(1)
+
+    orf_df, gt_map = flatten_orfs(df, args.gtf)
+    bed12_df = bed6_to_bed12(orf_df)
+    dotseq_gff = bed12_to_gtf(bed12_df, gt_map)
+
+    dotseq_gff.to_csv(f"{args.output}.gtf", sep="\t", header=False, index=False,
+                      quoting=csv.QUOTE_NONE, quotechar='')
+    orf_df.to_csv(f"{args.output}.bed", sep="\t", header=False, index=False)
+    
+if __name__ == "__main__":
+    main()
